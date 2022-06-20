d34144bc5d639220c043747b5dc903cb5e2c5d42
Cally Munro <callymunro@hotmail.co.uk>
2022-06-20 14:30:32 +0100

    20.06 - TypeI error of simple 2 arm parallel sim.

--- Diss2022 Initial Planning.R
+++ Diss2022 Initial Planning.R
@@ -1,6 +1,39 @@
 # Dissertation 2022: Initial Simple Simulation.
 
-# Git1 Testing:
-a <- 1
-b <- 2
-a + b
+
+# Initial calculation of one stage sample size.
+Sample_Size <- function(alpha, beta, delta, sigma, attr) {
+  n <- (qnorm(1 - alpha) - qnorm(beta))^2 * (2 * sigma^2) / delta ^ 2
+  n_adj <- n * 1/ (1 - attr) 
+  return(c(n, n_adj))
+}
+
+# Initial calculation of one stage sample size - BINOMIAL
+Sample_Size_Bin <- function(alpha, beta, delta, theta) {
+  n <- (qnorm(1 - alpha) - qnorm(beta))^2 * theta * (1 - theta) / delta ^ 2
+  n_adj <- n * 1/ (1 - attr) 
+  return(c(n, n_adj))
+}
+
+# Simulation for simple binary endpoint.
+# Need a null hypothesis:
+  # H0: delta = 0
+  # H1: delta /ne 0
+
+
+# In terms of a function (need to generate under the null hypothesis):
+TypeI_Error <- function(alpha, beta, delta, sigma, mu, n) {
+  # Generate data from the normal distribution.
+  x <- rnorm(n)
+  
+  # Calculate the one sample t-test value for each data.
+  ttest <- (x - mu) / (sigma / sqrt(n))
+  
+  # Critical value of the normal distribution.
+  critval <- qnorm(alpha)
+  
+  # TypeI error: Rejecting the null when it is true.
+  TypeI <- sum((ttest <= qnorm(alpha)) / n)
+  
+  return(TypeI)
+}
